c03c94e0b234a254adb4df63cbb10536d4a53a68
KharagSinghb <kharag94@gmail.com>
2024-06-11 13:59:55 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# NumberReader
+This program takes in a number and then returns if the number is odd, even or prime
+## Resources
+https://www.w3schools.com/java/java_conditions.asp
---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.Kharag","l":"MyNumber"},{"p":"com.Kharag","l":"NumberReader"}];updateSearchResults();
